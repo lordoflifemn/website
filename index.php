bd7f9968169b660de2a6cdcbef447d00b8bd67e2
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-05 12:40:01 -0600

    Updated home page slider box to have an info section to the right (moved Quick Links for now)

--- index.php
+++ index.php
@@ -13,31 +13,53 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="grid_12">
-					<!-- slider -->
-					<div class="slide_box">
-	                    <div class="camera_wrap" id="slider">
-		                    <div data-src="/images/slide1.jpg" data-thumb="/images/slide1.jpg">
-			                    <div class="camera_caption">
-				                    <h4>A sacred space </h4>
-				                    <h3>for everyone </h3>
-			                    </div>
-		                    </div>
-		                    <div data-src="/images/slide2.jpg" data-thumb="/images/slide2.jpg">
-			                    <div class="camera_caption">
-				                    <h4>Striving to help you </h4>
-				                    <h3>connect to Jesus </h3>
-			                    </div>
-		                    </div>
-		                    <div data-src="/images/slide3.jpg" data-thumb="/images/slide3.jpg">
-			                    <div class="camera_caption">
-				                    <h4>Reaching the world </h4>
-				                    <h3>for Christ</h3>
-			                    </div>
-		                    </div>
-	                    </div>
+                <div class="grid_12">
+                    <div class="slide_box">
+				        <div class="grid_8">
+					        <!-- slider -->
+	                        <div class="camera_wrap" id="slider">
+		                        <div data-src="/images/slide1.jpg" data-thumb="/images/slide1.jpg">
+			                        <div class="camera_caption">
+				                        <h4>A sacred space </h4>
+				                        <h3>for everyone </h3>
+			                        </div>
+		                        </div>
+		                        <div data-src="/images/slide2.jpg" data-thumb="/images/slide2.jpg">
+			                        <div class="camera_caption">
+				                        <h4>Striving to help you </h4>
+				                        <h3>connect to Jesus </h3>
+			                        </div>
+		                        </div>
+		                        <div data-src="/images/slide3.jpg" data-thumb="/images/slide3.jpg">
+			                        <div class="camera_caption">
+				                        <h4>Reaching the world </h4>
+				                        <h3>for Christ</h3>
+			                        </div>
+		                        </div>
+	                        </div>
+				        </div>
+                        <div class="grid_4">
+                            <!-- Quick Links -->
+						    <div class="title_box">
+							    <h4>
+								    Quick<span> Links</span>
+							    </h4>
+						    </div>
+                            <h6 class="icon_title">
+                                <a target="building_use" href="/forms/building_use_request.pdf">Building Use Request</a>
+                            </h6>
+                            <h6 class="icon_title">
+                                <a target="online_giving" href="https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=tYgT1GfNxRUldiimjHMvOaDUNE_2n1aV010wIx6-w5W6LyvD9eZ_F1OGVLl-PCpSums9tj3wI7okSf0MyMa8urOwf8eq3EVxVGM3f4X8K3Y=&ver=3">Online Giving</a>
+                            </h6>
+                            <h6 class="icon_title">
+                                <a target="volunteer_calendar" href="/forms/volunteer_calendars/2015_11_november.pdf">Volunteer Calendar</a>
+                            </h6>
+                            <h6 class="icon_title">
+                                <a target="church_calendar" href="http://www.google.com/calendar/embed?src=p6paf1v8o9kcen41ec0q6mraps%40group.calendar.google.com&ctz=America/Chicago">Church Calendar</a>
+                            </h6>                   
+                        </div>
                     </div>
-				</div>
+                </div>
 			</div>
 		</div>
 	</header>
@@ -75,35 +97,14 @@
 				
                 <div class="grid_4">
                     <!-- Worship Schedule -->
-					<div class="box1">
-						<div class="title_box">
-							<h4>
-								Worship<span> Schedule</span>
-							</h4>
-						</div>
-                        <?php include("/worship_schedule.html"); ?>
-					</div>
-
-                    <!-- Quick Links -->
-					<div class="box1">
-						<div class="title_box">
-							<h4>
-								Quick<span> Links</span>
-							</h4>
-						</div>
-                        <h6 class="icon_title">
-                            <a target="building_use" href="/forms/building_use_request.pdf">Building Use Request</a>
-                        </h6>
-                        <h6 class="icon_title">
-                            <a target="online_giving" href="https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=tYgT1GfNxRUldiimjHMvOaDUNE_2n1aV010wIx6-w5W6LyvD9eZ_F1OGVLl-PCpSums9tj3wI7okSf0MyMa8urOwf8eq3EVxVGM3f4X8K3Y=&ver=3">Online Giving</a>
-                        </h6>
-                        <h6 class="icon_title">
-                            <a target="volunteer_calendar" href="/forms/volunteer_calendars/2015_11_november.pdf">Volunteer Calendar</a>
-                        </h6>
-                        <h6 class="icon_title">
-                            <a target="church_calendar" href="http://www.google.com/calendar/embed?src=p6paf1v8o9kcen41ec0q6mraps%40group.calendar.google.com&ctz=America/Chicago">Church Calendar</a>
-                        </h6>
-					</div>
+                    <div class="box1">
+				        <div class="title_box">
+					        <h4>
+						        Worship<span> Schedule</span>
+					        </h4>
+				        </div>
+                        <?php include("/worship_schedule.html"); ?> 
+                    </div>
 
                     <!-- Upcoming Events -->
 					<div class="box1">
