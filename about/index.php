b093f18de262f48789dbc0c7352d77d8cad6c901
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-05 10:54:08 -0600

    Update about us index page and added our pastor

--- about/index.php
+++ about/index.php
@@ -14,38 +14,62 @@
 	<!-- content -->
 	<div id="content">
 		<div class="container">
+            <div class="row">
+                <div class="grid_12">
+                    <div class="box_crumb"><a href="/">Home</a> > About Us</div>
+                </div>
+            </div>
 			<div class="row">
-				<div class="grid_8">
+                <div class="grid_9">
 					<div class="box1">
 						<div class="title_box m_14">
 							<h4>
-								Global 
-								<span>Mission</span>
+								About 
+								<span>Us</span>
 							</h4>
 						</div>
-						
 						<div class="wrapper">
-							<img src="/images/page2_img1.jpg" alt="" class="img_fleft">
-							<div class="extra_wrap">								
-								<h6 class="italic">
-									Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
-								</h6>
-								<p>
-									Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod 
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-									veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-									ex reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-									nulla pariatur. Excepteur sint occaecat cupidproident, 
-									sunt in culpa qui officia.
-								</p>
-								<a href="#" class="btn">
-									<span>
-										Read more
-									</span>
-								</a>
+							<img src="/images/page3_img4.jpg" alt="" class="img_right">
+							<div class="extra_wrap">
+                                <h6 class="italic">
+	                                What do people say about our church? 
+                                </h6>
+                                <p>
+	                                Most people find Lord of Life to be a friendly and welcoming, come as you are, accepted for who you are, give of your 
+                                    talents and leadership, grow together in spirit and faith, give as God leads you, lean on each other place, where 
+                                    traditions are alive, music rings out, and the hope and promise and grace of Jesus Christ are made known and celebrated!
+                                    You will find additional information in our Visitor Guide.
+                                </p>
+                                <p>
+                                    Our church is not a building; it is group of regular people that have come together. United we work to form a family.
+                                    We are a diverse gathering with a common hope - to become closer to our faith through ministry and fellowship.
+                                </p>
 							</div>
 						</div>
 					</div>
+                    <div class="box1">
+						<div class="title_box m_14">
+							<h4>
+								Our 
+								<span>History</span>
+							</h4>
+						</div>
+                        <p>
+                            Lord of Life Lutheran Church is an ELCA congregation serving Farmington, Apple Valley, Lakeville, Rosemount and the 
+                            surrounding communities. Lord of Life started in the fall of 1994 to help meet the spiritual needs of people 
+                            searching for a home church, and to provide a ministry to people moving into the community. Our first service was 
+                            held in April 1995 and in April 1997, Lord of Life became an organized church of The Evangelical Lutheran Church 
+                            in America.
+                        </p>
+                        <p>
+                            We are committed to hearing and sharing God's promise as God speaks to us, guides us, and inspires us to answer our 
+                            call of discipleship and mission to make God first in our lives.
+                        </p>
+                        <p>
+                            We are proud members of the <a href="http://www.spas-elca.org/" target="_blank">Saint Paul Area Synod</a> and the 
+                            <a href="http://www.elca.org/" target="_blank">Evangelical Lutheran Church in America</a>.
+                        </p>
+                    </div>
 					<div class="box1">
 						<div class="title_box">
 							<h4>
@@ -113,7 +137,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="grid_4">
+                <div class="grid_3">
 					<div class="box2">
 						<div class="title_box">
 							<h4>
---
+++ about/meet_our_pastor.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Lord of Life | About</title>
+	    <?php include($_SERVER["DOCUMENT_ROOT"] . "/bootstrap.html"); ?>
+    </head>
+    <body>
+        <!-- header -->
+	    <header>
+		    <?php include($_SERVER["DOCUMENT_ROOT"] . "/header.html"); ?>
+	    </header>
+
+    </body>
+</html>
