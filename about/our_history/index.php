de6a962ae46cfbcb7b967a0723b3efe716310e95
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-06 08:06:21 -0600

    Updated 'About Us' to have its menu in the upper right and added structure for additional pages - Core Values, Meet Our Pastor and Our History.

--- about/our_history/index.php
+++ about/our_history/index.php
@@ -16,39 +16,11 @@
 		<div class="container">
 			<div class="row">
                 <div class="grid_9">
-                    <!-- About Us -->
-					<div class="box1">
-						<div class="title_box m_14">
-							<h4>
-								About 
-								<span>Us</span>
-							</h4>
-						</div>
-						<div class="wrapper">
-							<img src="/images/page3_img4.jpg" alt="" class="img_right">
-							<div class="extra_wrap">
-                                <h6 class="italic">
-	                                What do people say about our church? 
-                                </h6>
-                                <p>
-	                                Most people find Lord of Life to be a friendly and welcoming, come as you are, accepted for who you are, give of your 
-                                    talents and leadership, grow together in spirit and faith, give as God leads you, lean on each other place, where 
-                                    traditions are alive, music rings out, and the hope and promise and grace of Jesus Christ are made known and celebrated!
-                                    You will find additional information in our Visitor Guide.
-                                </p>
-                                <p>
-                                    Our church is not a building; it is group of regular people that have come together. United we work to form a family.
-                                    We are a diverse gathering with a common hope - to become closer to our faith through ministry and fellowship.
-                                </p>
-							</div>
-						</div>
-					</div>
                     <!-- Our History -->
                     <div class="box1">
 						<div class="title_box m_14">
 							<h4>
-								Our 
-								<span>History</span>
+								Our <span>History</span>
 							</h4>
 						</div>
                         <p>
@@ -67,72 +39,6 @@
                             <a href="http://www.elca.org/" target="_blank">Evangelical Lutheran Church in America</a>.
                         </p>
                     </div>
-					<div class="box1">
-						<div class="title_box">
-							<h4>
-								Our 
-								<span>Philosophy</span>
-							</h4>
-						</div>
-						<div class="title_box">
-							<div class="qoute_box">
-								<div class="qoute_number">
-									<span>
-										1.
-									</span>
-								</div>
-								<div class="quote_content">
-									<h6 class="italic">
-										<a href="#">Nam ullamcorper blandit tortor</a>
-									</h6>
-									<p>
-										Suspendisse potenti. Nunc tempus vulputate sapien, eget lobortis risus vulputate vitae. 
-										Quisque iaculis elit sed lorem adipiscing euismod. Aenean ut ornare lectus. 
-										Donec sodales nec odio nec mollis. Suspendisse non augue convallis, 
-										convallis nunc eu, placerat lorem. 
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="title_box">
-							<div class="qoute_box">
-								<div class="qoute_number">
-									<span>
-										2.
-									</span>
-								</div>
-								<div class="quote_content">
-									<h6 class="italic">
-										<a href="#">Sit amet conse ctetur adipisicing</a>
-									</h6>
-									<p>
-										Suspendisse potenti. Nunc tempus vulputate sapien, eget lobortis risus vulputate vitae. 
-										Quisque iaculis elit sed lorem adipiscing euismod. Aenean ut ornare lectus. 
-										Donec sodales nec odio nec mollis. Suspendisse non augue convallis, 
-										convallis nunc eu, placerat lorem. 
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="qoute_box">
-							<div class="qoute_number">
-								<span>
-									3.
-								</span>
-							</div>
-							<div class="quote_content">
-								<h6 class="italic">
-									<a href="#">Mauris mollis nisl id auctor</a>
-								</h6>
-								<p>
-									Suspendisse potenti. Nunc tempus vulputate sapien, eget lobortis risus vulputate vitae. 
-									Quisque iaculis elit sed lorem adipiscing euismod. Aenean ut ornare lectus. 
-									Donec sodales nec odio nec mollis. Suspendisse non augue convallis, 
-									convallis nunc eu, placerat lorem. 
-								</p>
-							</div>
-						</div>
-					</div>
 				</div>
                 <div class="grid_3">
                     <!-- About Us Menu -->
