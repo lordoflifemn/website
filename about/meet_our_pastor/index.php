093cf63d1fd4a65da977de9682e58ee1fe13b8d6
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-07 21:30:35 -0600

    Added content for meet our pastor page

--- about/meet_our_pastor/index.php
+++ about/meet_our_pastor/index.php
@@ -24,10 +24,41 @@
 							</h4>
 						</div>
 						<div class="wrapper">
-							<div class="extra_wrap">
+                            <div class="extra_wrap">
                                 <h6 class="italic">
-	                                [Enter a title]? 
+	                                Pastor Jamie Thompson
                                 </h6>
+                                <img src="/images/jamie_thompson.jpg" class="img_left" style="float: left; margin-right: 10px;">
+                                <p>
+                                    I have served the greater Twin Cities during my vocational life. I served as a youth director in the Glen Lake area of
+                                    Minnetonka for a couple years, as a youth director on the near northeast side of Minneapolis while attending seminary,
+                                    my internship was in Marine On St. Croix north of Stillwater, my fist call was King of Kings in Woodbury, and in August
+                                    1994, I was appointed by Division For Outreach to start a new ministry to serve north Farmington, eastern Lakeville.
+                                    My wife, Sherri, works for Cargill, Inc. We were married in June 1986. Our son, Nate, is 21, a 4th year
+                                    Communications student at The University of Minnesota, and a full time employee at the airport, serving Delta and
+                                    Regional Services. Our daughter, Bre, is 19, and a first year student at Hamline, involved on campus on many levels,
+                                    pursuing business, preparing to study abroad in York, England. Family and community are both important to
+                                    me. My mother, my sister and her family, and my brother and his family, and most of Sherri’s extended family, all live
+                                    within the greater metro area. I enjoy the outdoors and staying active when possible. My goals for continuing
+                                    education include pursuing a doctorate degree, and writing in the future.
+                                </p>
+                                <p>
+                                    My vision for Lord of Life includes, first, staying faithful and honoring the amazing people of God and the mission and
+                                    grace, God has brought together at this time, through this ministry. I believe God is forever leading us forward,
+                                    making God first in many new ways, inviting us to always trust God is leading us in how we will next be asked to
+                                    further develop our facility and staff. God continues to move us, and to move through us, to grow towards possible
+                                    ventures which include: 7 Day A Week Worship, Small Group Type Ministry Relationships and Service, After School
+                                    Ministry, Advancing The Performing Arts, Community Based Recreation Play & Exercise, Community Space To Support
+                                    The Efforts Of Others, keeping us forever open to exploring and welcoming how The Lord of our life is calling us and
+                                    blessing us to make God first, through local and global ministry and mission events.
+                                </p>
+                                <p>
+                                    I am connected to and committed to our greater community in a variety of settings: Volunteer Police Chaplain for The
+                                    Farmington Police Department; A Member Of The Farmington School District Trauma Response Team; Member Of The
+                                    Farmington Ministerial Association; Member Of The St. Paul Area Synod Council; Board Member Of The Trinity Care
+                                    Center Campus, Farmington; Member Of Local Rotary Club Of Farmington.
+                                </p>
+                                <a href="mailto:jamie.thompson@lordoflifemn.org">Contact Jamie</a>
 							</div>
 						</div>
 					</div>
