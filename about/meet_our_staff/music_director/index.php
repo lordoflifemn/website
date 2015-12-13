8192cf71350c4c8b24cf165503a07e4132e46206
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-13 15:01:31 -0600

    Added meet our staff section - added admin assistant and music director

---
+++ about/meet_our_staff/music_director/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>Lord of Life | About</title>
+	<?php include($_SERVER["DOCUMENT_ROOT"] . "/bootstrap.html"); ?>
+</head>
+<body>
+    <!-- header -->
+	<header>
+		<?php include($_SERVER["DOCUMENT_ROOT"] . "/header.html"); ?>
+	</header>
+
+    <!-- content -->
+	<div id="content">
+		<div class="container">
+			<div class="row">
+                <div class="grid_9">
+                    <!-- Core Values -->
+					<div class="box1">
+						<div class="title_box m_14">
+							<h4>
+								Our <span>Music Director</span>
+							</h4>
+						</div>
+					</div>
+				</div>
+                <div class="grid_3">
+                    <!-- About Menu -->
+                    <div class="box1">
+                        <?php include($_SERVER["DOCUMENT_ROOT"] . "/about/menu.html"); ?>
+                    </div>
+					<div class="box2">
+						<div class="title_box">
+							<h4>
+								Online Donations. <span>Real Help to People!</span>
+							</h4>
+						</div>
+						<h6 class="italic m_01">
+							<a href="#">
+								Ctetur adipisicing elit, sed do eiusmod tempor incididunt
+							</a>
+						</h6>
+						<p class="m_0">
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+						</p>
+					</div>
+					<div class="box1">
+						<div class="title_box">
+							<h4>
+								What <span> We Believe In</span>
+							</h4>
+						</div>
+						<div class="title_box">						
+							<h6 class="icon_title">
+								<a href="#">Sit amet conse ctetur adipisicing</a>
+							</h6>
+							<p>
+								Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididu
+							</p>
+						</div>
+						<div class="title_box">
+							<h6 class="icon_title">
+								<a href="#">Officia deserunt mollit </a>
+							</h6>
+							<p>
+								Amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+							</p>
+						</div>
+						<div class="title_box">
+							<h6 class="icon_title">
+								<a href="#">Amet conse ctetur adipisicing</a>
+							</h6>
+							<p>
+								Conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+							</p>
+						</div>
+						<h6 class="icon_title">
+							<a href="#">Anim id est laborum</a>
+						</h6>
+						<p class="m_0">
+							Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <!-- footer -->
+	<footer>
+		<?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.html"); ?>
+	</footer>
+</body>
+</html>
