5221fd675bd9c084e92f8b3e6c34995656cc9d04
lordoflifemn <chad.kalstabakken@gmail.com>
2015-12-14 06:33:18 -0600

    Added content for education

--- education/index.php
+++ education/index.php
@@ -19,74 +19,137 @@
 					<div class="box1">
 						<div class="title_box m_11">
 							<h4>
-								Educational
-								<span> Opportunities</span>
+								Educational <span>Opportunities</span>
 							</h4>
 						</div>
 						<div class="title_box m_13">
 							<div class="wrapper">
-								<img src="/images/page3_img4.jpg" alt="" class="img_fleft">
+                                <!-- Sunday School & Life Nite -->
 								<div class="extra_wrap">
-									
 									<h6 class="italic">
-										<a href="">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut</a>
+										Faith Quest Sunday School & Life Nite
 									</h6>
+                                    <img src="/images/page3_img5.jpg" alt="" class="img_right" style="float: right; margin-left: 10px;">
 									<p>
-										Vivamus eget semper elit, sit amet ornare risus. Proin venenatis fermentum massa, at dignissim erat! 
-										Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-										Nam ornare velit justo, sit amet auctor turpis faucibus elementum. Aenean tempor, 
-										sem sagittis bibendum interdum, magna ullamcorper ante, quis sagittis.
+										Faith Quest, our Sunday School Program is held September - May during our 9:30 am worship service. All 
+                                        children and youth ages 4 years through 6th grade are welcome to attend and may enroll at any time.
 									</p>
-									
+									<p>
+                                        Faith Quest classes are formed according to age/grade level and have an emphasis on Bible stories, crafts, 
+                                        songs and community projects.
+                                    </p>
+                                    <p>
+                                        Life Nite (6th-9th grade classes) is the first Wednesday of each month, October-May; 6-7:30 p.m.
+                                    </p>
+                                    <p>
+                                        New children (and additional teachers) are accepted at any time during the year for Sunday School. If you 
+                                        are interested in enrolling your child(ren), please print out our brochure and return the registration 
+                                        portion to the church office when complete.
+                                    </p>
+                                    <div style="float: left; margin-top: -5px;">
+                                        <a href="/forms/FQ_Life_Nite_Registration_Brochure_2015-2016.pdf">Faith Quest & Life Nite Registration Form</a><br>
+                                        <a href="/forms/Defining_Youth_Ministries_September_2014.pdf">Defining Youth Ministries</a>
+                                    </div>
+                                    <div style="float: left; margin-left: 30px; margin-top: -5px;">
+                                        <a href="/forms/FQ_2015_Fall_Newsletter.pdf">Faith Quest Fall 2015 Newsletter</a><br>
+                                        <a href="/forms/FQ_Newsletter_Christmas_2015.pdf">Faith Quest Christmas 2015 Newsletter</a>
+                                    </div>
+                                </div>
+                                <!-- Vacation Bibile School -->
+                                <div class="extra_wrap" style="margin-top: 15px;">
+                                    <h6 class="italic" style="clear: both;">
+										Vacation Bible School
+									</h6>
+                                    <p>
+                                        Vacation Bible School is held in the summer. All children ages 3 years old and up are 
+                                        welcome to attend. Questions, please email <a href="mailto:jamie.thompson@lordoflifemn.org">Pastor Jamie</a>.
+                                        2016 VBS dates are July 11 - 15, from 9:30 - 11:30 a.m.
+                                    </p>
 								</div>
 							</div>
 						</div>
-						<div class="wrapper">
-							<img src="/images/page3_img5.jpg" alt="" class="img_fleft">
-							<div class="extra_wrap">
-								
-								<h6 class="italic">
-									<a href="">Dolor sit amet conse ctetur adipisicing </a>
-								</h6>
-								<p>
-									Vivamus eget semper elit, sit amet ornare risus. Proin venenatis fermentum massa, at dignissim erat! 
-									Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-									Nam ornare velit justo, sit amet auctor turpis faucibus elementum. Aenean tempor, 
-									sem sagittis bibendum interdum, magna ullamcorper ante, quis sagittis.
-								</p>
-								
-							</div>
-						</div>
+                        <div class="title_box m_13">
+						    <div class="wrapper">
+                                <!-- Communion -->
+							    <div class="extra_wrap">
+								    <h6 class="italic">
+									    Communion
+								    </h6>
+                                    <p>
+									    We offer a first Communion instruction class annually during Lent for 4th 
+                                        graders and up.  First Communion is celebrated each year on Maundy Thursday.
+                                    </p>
+                                    <p>
+                                        The 2016 First Communion instruction class will be on a Saturday in early 
+                                        March 14, likely 10 a.m. - 12:30 p.m. 
+								    </p>
+                                    <a href="/forms/First_Communion_Registration_2015.pdf">First Communion Registration Form</a>
+							    </div>
+						    </div>
+                        </div>
+                        <div class="title_box m_13">
+                            <div class="wrapper">
+                                <!-- Confirmation -->
+							    <div class="extra_wrap">
+								    <h6 class="italic">
+									    Confirmation
+								    </h6>
+                                    <p>
+									    Confirmation classes are offered for 7th-9th grade students. 
+                                        <a href="/forms/Confirmation%20Manual%202011%202012.pdf">Confirmation program manual</a>
+                                        available online. If interested, please contact <a href="mailto:jamie.thompson@lordoflifemn.org">Pastor Jamie</a>.  
+                                    </p>
+                                    <p>
+                                        Confirmation classes are generally:
+                                        <ul style="margin-left: 10px; margin-top: -15px;">
+                                            <li>- 7th grade on the first Sunday of the month</li>
+                                            <li>- 8th grade on the second Sunday of the month</li>
+                                            <li>- 9th grade on the third Sunday of the month</li>
+                                        </ul>
+                                        Check church calendar for exact times.
+								    </p>
+							    </div>
+						    </div>
+                        </div>
+                        <div class="title_box m_13">
+                            <div class="wrapper">
+                                <!-- Adult -->
+							    <div class="extra_wrap">
+								    <h6 class="italic">
+									    Adult
+								    </h6>
+                                    <p>
+									    We have many small groups available: Women's Ministries, Men's Ministries, Women & Girls, plus seasonal Bible studies. Watch 
+                                        for more information on our next adult Bible study.
+                                    </p>
+							    </div>
+						    </div>
+                        </div>
 					</div>
 				</div>
 				<div class="grid_4">
 					<div class="box1">
-						<div class="title_box m_10">
+						<div class="title_box">
 							<h4>
-								Financial <br>
-								<span> Resources</span>
+								Links <span>& Registrations</span>
 							</h4>
 						</div>
-						
-						<h6 class="italic">
-							Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
-						</h6>
-						
-						<p>
-							<span>
-								Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor 
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-								fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
-							</span>
-						</p>
-						
-						<a href="#" class="btn">
-							<span>
-								Read more
-							</span>
-						</a>
+                        <div class="title_box">
+                            <h6 class="icon_title">
+	                            <a href="/forms/FQ_Life_Nite_Registration_Brochure_2015-2016.pdf">Faith Quest & Life Nite Registration Form</a>
+                            </h6>
+                            <h6 class="icon_title">
+                                <a href="/forms/FQ_2015_Fall_Newsletter.pdf">Faith Quest Fall 2015 Newsletter</a>
+                            </h6>
+                            <h6 class="icon_title" style="margin-bottom: 10px;">
+                                <a href="/forms/FQ_Newsletter_Christmas_2015.pdf">Faith Quest Christmas 2015 Newsletter</a>
+                            </h6>
+                        </div>
+                        <div class="title_box">
+                            <h6 class="icon_title" style="margin-bottom: 10px;">
+	                            <a href="/forms/First_Communion_Registration_2015.pdf">First Communion Registration Form</a>
+                            </h6>
+                        </div>
 					</div>
 				</div>
 			</div>
